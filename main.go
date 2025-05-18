12f4914e5299f89df071e3c9365667b70c2e5d0c
Jervis Muindi <jervisfm@users.noreply.github.com>
2025-05-18 05:32:19 +0000

    Add logic for using tools into agent loop.

--- main.go
+++ main.go
@@ -95,32 +95,70 @@ func (agent *Agent) runInference(ctx context.Context, conversation []anthropic.M
 	return message, err
 }
 
+func (agent *Agent) executeTool(id, name string, input json.RawMessage) anthropic.ContentBlockParamUnion {
+	var toolDef ToolDefinition
+	var found bool
+	for _, tool := range agent.tools {
+		if tool.Name == name {
+			toolDef = tool
+			found = true
+			break
+		}
+	}
+	if !found {
+		return anthropic.NewToolResultBlock(id, "tool not found", true)
+
+	}
+	fmt.Printf("\u001b[92mtool\u001b[0m: %s(%s)\n", name, input)
+	response, err := toolDef.Function(input)
+	if err != nil {
+		return anthropic.NewToolResultBlock(id, err.Error(), true)
+	}
+	return anthropic.NewToolResultBlock(id, response, false)
+}
+
 func (agent *Agent) Run(ctx context.Context) error {
 	conversation := []anthropic.MessageParam{}
 
 	fmt.Println("Chat with Claude AI Agent (use 'Ctrl-C' to quit)")
+	readUserInput := true
 	for {
-		fmt.Print("\u001b[94mYou\u001b[0m: ")
-		userInput, ok := agent.getUserMessage()
-		if !ok {
-			break
+		if readUserInput {
+			fmt.Print("\u001b[94mYou\u001b[0m: ")
+			userInput, ok := agent.getUserMessage()
+			if !ok {
+				break
+			}
+
+			userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
+			conversation = append(conversation, userMessage)
 		}
-
-		userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
-		conversation = append(conversation, userMessage)
-
 		message, err := agent.runInference(ctx, conversation)
 		if err != nil {
 			return err
 		}
 		conversation = append(conversation, message.ToParam())
 
+		toolResults := []anthropic.ContentBlockParamUnion{}
+
 		for _, content := range message.Content {
 			switch content.Type {
 			case "text":
 				fmt.Printf("\u001b[93mClaude\u001b[0m: %s\n", content.Text)
+
+			case "tool_use":
+				result := agent.executeTool(content.ID, content.Name, content.Input)
+				toolResults = append(toolResults, result)
 			}
 		}
+
+		if len(toolResults) == 0 {
+			readUserInput = true
+			continue
+		}
+		// Otherwise, we have tool results to process
+		readUserInput = false
+		conversation = append(conversation, anthropic.NewUserMessage(toolResults...))
 	}
 
 	return nil
