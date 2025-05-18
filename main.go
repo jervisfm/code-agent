f982c9741474baf4963aafb483c1f1080bdaa97f
Jervis Muindi <jervisfm@users.noreply.github.com>
2025-05-18 04:10:43 +0000

    Add a basic agent starter code and install claude api

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/anthropics/anthropic-sdk-go"
+)
+
+type Agent struct {
+	client          *anthropic.Client
+	getUserMesssage func() (string, bool)
+}
+
+func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool)) *Agent {
+	return &Agent{
+		client:         client,
+		getUserMessage: getUserMesssage,
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	client := anthropic.NewClient()
+	scanner := bufio.NewScanner(os.Stdin)
+	getUserMesssage := func() (string, boo) {
+		if !scanner.Scan() {
+			return "", false
+		}
+		return scanner.Text(), true
+	}
+
+	agent := NewAgent(&client, getUserMesssage)
+	err := agent.Run(context.TODO())
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+	}
+
 }
