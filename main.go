e5cedbc59ab988b92c2b15ca454fad83fb1ab5bf
Jervis Muindi <jervisfm@users.noreply.github.com>
2025-05-18 04:24:28 +0000

    Fix some typos in code

--- main.go
+++ main.go
@@ -55,14 +55,14 @@ func (agent *Agent) Run(ctx context.Context) error {
 func main() {
 	client := anthropic.NewClient()
 	scanner := bufio.NewScanner(os.Stdin)
-	getUserMesssage := func() (string, boo) {
+	getUserMessage := func() (string, bool) {
 		if !scanner.Scan() {
 			return "", false
 		}
 		return scanner.Text(), true
 	}
 
-	agent := NewAgent(&client, getUserMesssage)
+	agent := NewAgent(&client, getUserMessage)
 	err := agent.Run(context.TODO())
 	if err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
