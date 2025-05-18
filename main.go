4e2b6476de6e73f89636d2ddd4ef3984d47d38f2
Jervis Muindi <jervisfm@users.noreply.github.com>
2025-05-18 04:22:34 +0000

    Add agent run method

--- main.go
+++ main.go
@@ -21,6 +21,37 @@ func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool)) *A
 	}
 }
 
+func (agent *Agent) Run(ctx context.Context) error {
+	conversation := []anthropic.MesssgeParam{}
+
+	fmt.Println("Chat with Claude AI Agent (use 'Ctrl-C' to quit)")
+	for {
+		fmt.Print("\u001b[94mYou\u001b[0m: ")
+		userInput, ok := agent.getUserMessage()
+		if !ok {
+			break
+		}
+
+		userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
+		conversation = append(conversation, userMessage)
+
+		message, err := agent.runInference(ctx, conversation)
+		if err != nil {
+			return err
+		}
+		conversation = append(conversation, message.ToParam())
+
+		for _, content := message.Content {
+			switch content.Type {
+			case anthropic.MessageTypeText:
+				fmt.Printf("\u001b[93mClaude\u001b[0m: %s\n", content.Text)
+			}
+		}
+	}
+
+	return nil
+}
+
 func main() {
 	client := anthropic.NewClient()
 	scanner := bufio.NewScanner(os.Stdin)
