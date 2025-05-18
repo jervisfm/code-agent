2341f831c700d5bab96e1e9896e3844e0994edc5
Jervis Muindi <jervisfm@users.noreply.github.com>
2025-05-18 06:30:07 +0000

    Add fizz buzz code gen by the agent

---
+++ FizzBuzz.java
@@ -0,0 +1,16 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        // Print FizzBuzz from 1 to 42
+        for (int i = 1; i <= 42; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
